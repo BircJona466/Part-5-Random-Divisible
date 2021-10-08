9cac365f1c9ad778ad516d35ea7184c56e0149d0
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-08 08:18:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5_Random_Divisible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+
+            int numInput;
+            int randNum = generator.Next(2, 6);
+
+            Console.WriteLine(randNum);
+            Console.WriteLine(" Please input a higher number ");
+
+
+            if (int.TryParse(Console.ReadLine(), out numInput))
+            {
+
+                Console.WriteLine((numInput % randNum));
+                int total = Convert.ToInt32((numInput % randNum));
+
+
+                if (numInput < randNum)
+                {
+                    Console.WriteLine("That Number is not higher.");
+                }
+
+                else if (numInput <= 0)
+                {
+                    Console.WriteLine("That is not a number.");
+                }
+
+                else if (total == 0)
+                {
+                    Console.WriteLine("It is divisable.");
+                }
+                Console.ReadLine();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
